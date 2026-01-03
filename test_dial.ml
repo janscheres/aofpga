8e6d92a3c9b0dc3f849cc62e02d12c3f28f42369
Jan Scheres <jscheresm@gmail.com>
2026-01-03 15:12:37 +0000

    Fix bug in test

--- test_dial.ml
+++ test_dial.ml
@@ -4,7 +4,6 @@ module Sim = Cyclesim.With_interface(Dial.I)(Dial.O)
 
 let () =
   let sim = Sim.create (Dial.create (Scope.create ())) in
-  (* Note: We removed the waveform viewer here to make it run fast! *)
   
   let i = {
     Dial.I.
@@ -23,7 +22,7 @@ let () =
   } in
 
   let move dir_str amt =
-    (* Convert "L"/"R" string to 0 or 1 *)
+    (* turn L/R to 0 or 1 *)
     let dir_bit = if String.equal dir_str "R" then 1 else 0 in
     
     i.start := Bits.one 1;
@@ -39,12 +38,12 @@ let () =
     Cyclesim.cycle sim
   in
 
-  (* 1. Reset *)
+  (* reset *)
   i.clear := Bits.one 1;
   Cyclesim.cycle sim;
   i.clear := Bits.zero 1;
 
-  (* 2. Open the file and loop through lines *)
+  (* loop thru lines in file *)
   let filename = "input.txt" in
   let chan = In_channel.open_text filename in
   
@@ -54,12 +53,12 @@ let () =
       match line with
       | None -> raise End_of_file
       | Some l -> 
-          (* Parse "L68" -> "L", 68 *)
+          (* seperate line into direction and amount *)
           let l = String.trim l in
           if String.length l > 0 then (
-            let dir = String.sub l 0 1 in
-            let amt = int_of_string (String.sub l 1 (String.length l - 1)) in
-            move dir amt
+            let dir = String.sub l 0 1 in (* from 0 index take one *)
+            let amt = int_of_string (String.sub l 1 (String.length l - 1)) in (* take everything from index 1 on*)
+            move dir amt (* use previously defined function to update *)
           )
     done
   with End_of_file ->
@@ -67,4 +66,5 @@ let () =
     
     (* 3. Print the Answer! *)
     Printf.printf "--- SOLVED ---\n";
-    Printf.printf "Part 1: %d\n" (Bits.to_int !(o.password))
+    Printf.printf "Part 1: %d\n" (Bits.to_int !(o.password));
+    Printf.printf "Part 2: %d\n" (Bits.to_int !(o.part2))
